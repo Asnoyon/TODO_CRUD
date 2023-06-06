0cb8557b6ab5bc8fa4ff20a1c5143444d2c108dd
Asnoyon <blackpain1100@gmail.com>
2023-06-07 00:24:44 +0600

    updae insert

--- insert.php
+++ insert.php
@@ -4,7 +4,7 @@
  $desc = $_POST['desc'];
  $prior = $_POST['prior'];
  $date = $_POST['date'];
- 
- mysqli_query($conn,"INSERT INTO `tbtodo`(`list`, `description`, `priority`, `date`) VALUES ('$list,'$desc',' $prior',' $date')");
+
+ mysqli_query($conn,"INSERT INTO `tbtodo`(`list`, `description`, `priority`, `date`) VALUES ('$list','$desc','$prior','$date')");
  header("location:index.php");
 ?>
