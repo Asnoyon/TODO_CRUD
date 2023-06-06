bb5287178e3da76f23a149a630ef44cc6abe2618
Asnoyon <blackpain1100@gmail.com>
2023-06-07 00:14:59 +0600

    update index

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Only Books</title>
+    <title>TODO LIST APP</title>
     <!-- font awesome online cdn -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <!-- Bootstrap 5 css -->
@@ -59,10 +59,10 @@
 
                 <tr>
                     <td class="py-3"> <?php echo $row['id']  ?> </td>
-                    <td class="py-3"></td>
-                    <td class="py-3"></td>
-                    <td class="py-3"></td>
-                    <td class="py-3"></td>
+                    <td class="py-3"> <?php echo $row['list']  ?></td>
+                    <td class="py-3"> <?php echo $row['description']  ?></td>
+                    <td class="py-3">  <?php echo $row['priority']  ?></td>
+                    <td class="py-3">  <?php echo $row['date']  ?></td>
                     <td style="width:10%;"><a href="delete.php" class="btn btn-outline-danger" >DELETE</a></td>
                     <td style="width:10%;"><a href="update.php" class="btn btn-outline-success" >UPDATE</a></td>
                 </tr>
